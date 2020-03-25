bd01a22c6000d150616197e5d58b52bbfed9a24e
Park, Jonghyeon <me@ho9.me>
2020-03-25 11:05:52 +0900

    머티리얼화 대상 확대, 토론 디자인 개선

--- js/materialize.js
+++ js/materialize.js
@@ -19,18 +19,53 @@ function materialize () {
             }
 
             let attributes = ''
+            let attributesKeys = []
             for(let j = 0; j < inputs[i].attributes.length; j++) {
+                attributesKeys.push(inputs[i].attributes[j].name)
                 if (inputs[i].attributes[j].name === 'class') {
-                    attributes = attributes + 'class="mdl-textfield__input ' + inputs[i].attributes[j].value + '"'
+                    attributes = attributes + ' class="mdl-textfield__input ' + inputs[i].attributes[j].value + '"'
                 } else {
                     attributes = attributes + ' ' + inputs[i].attributes[j].name + '="' + inputs[i].attributes[j].value + '"'
                 }
             }
+            if (attributesKeys.indexOf('class') < 0) {
+                attributes = attributes + ' class="mdl-textfield__input"'
+            }
 
             inputs[i].outerHTML = `
             <div class="mdl-textfield mdl-js-textfield">
-                <input class="mdl-textfield__input" ` + attributes + `>
+                <input ` + attributes + `>
                 <label class="mdl-textfield__label" for="` + inputs[i].id + `"></label>
+            </div>
+            `
+        }
+    }
+
+    let textareas = document.getElementsByTagName('textarea')
+    for (let i = 0; i < textareas.length; i++) {
+        if ((textareas[i].className == undefined || textareas[i].className == '')) {
+            if (textareas[i].id == undefined || textareas[i].id == '') {
+                textareas[i].id = 'textarea' + i
+            }
+
+            let attributes = ''
+            let attributesKeys = []
+            for(let j = 0; j < textareas[i].attributes.length; j++) {
+                attributesKeys.push(textareas[i].attributes[j].name)
+                if (textareas[i].attributes[j].name === 'class') {
+                    attributes = attributes + ' class="mdl-textfield__input ' + textareas[i].attributes[j].value + '"'
+                } else {
+                    attributes = attributes + ' ' + textareas[i].attributes[j].name + '="' + textareas[i].attributes[j].value + '"'
+                }
+            }
+            if (attributesKeys.indexOf('class') < 0) {
+                attributes = attributes + ' class="mdl-textfield__input"'
+            }
+
+            textareas[i].outerHTML = `
+            <div class="mdl-textfield mdl-js-textfield">
+                <textarea class="mdl-textfield__input" ` + attributes + `></textarea>
+                <label class="mdl-textfield__label" for="` + textareas[i].id + `"></label>
             </div>
             `
         }
